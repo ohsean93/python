79e6432534e803d299e0aaf983d49d9b298f669f
Oh Jeasuk <jeasuk93@gmail.com>
2019-07-16 15:58:56 +0900

    19_07_16_upload

---
+++ 파이썬_문제해결_기본/p1_simple_cal.py
@@ -0,0 +1,9 @@
+# incording : utf-8
+# p1_simple_cal.py
+
+a, b = map(int,input().split(" "))
+
+print('{0}'.format(a+b))
+print('{0}'.format(a-b))
+print('{0}'.format(a*b))
+print('{0}'.format(a//b))
